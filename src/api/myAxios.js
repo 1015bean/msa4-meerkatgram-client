66b08b18236f34725eb93ca5e256783460c40e41
1015bean <ibqi1691@gmail.com>
2026-05-29 17:42:01 +0900

    260529 [develop] 로그인 완성, 로그인 유효성 검사 완성, 로그인 리프레시 이슈 및 엑세스토큰 만료 대응 완료

--- src/api/myAxios.js
+++ src/api/myAxios.js
@@ -19,15 +19,17 @@ const myAxios = axios.create({
 });
 
 
-// .interceptors: 요청이 왔을 때 로직이 실행되기 전에 실행되는 함수
+// .interceptors~: 요청이 가는 중에, 원하는 로직이 실행되기 전에 먼저 실행되는 함수
+// config: 리퀘스트하는 정보가 포괄적으로 담긴 객체
 myAxios.interceptors.request.use(async (config) => {
   const authStore = useAuthStore();
   let accessToken = authStore.accessToken;
-  const denyUrl = /^\/api\/reissue-token$/;  // 리트라이 제외 URL 설정
+  // .reissue() 요청 가는중에 인터셉션(if~로직) 실행될 수 있으므로, 제외할 URL 설정
+  const denyUrl = /^\/api\/reissue-token$/;  
 
   if(!denyUrl.test(config.url) && authStore.isLoggedIn) {
     // 엑세스토큰 만료 확인(시간 비교)
-    // : 토큰 정보 추출 > 현재시간 획득 > 토큰 유효시간 획득(5분 빠르게) > 시간 비교
+      // : 토큰 정보 추출 > 현재시간 획득 > 토큰 유효시간 획득(5분 빠르게) > 시간 비교
     const claims = jwtDecode(accessToken);
     const now = dayjs().unix();
     const expTime = dayjs.unix(claims.exp).add(-5, 'minute').unix();
@@ -43,6 +45,7 @@ myAxios.interceptors.request.use(async (config) => {
     }
   }
 
+  // 인터셉터 처리는 끝났구요. 여기 엑세스토큰이랑 리쿼스트 정보 드릴게요 하려던 로직 실행하세요.
   config.headers.Authorization = `Bearer ${accessToken}`;
   return config;
 });
